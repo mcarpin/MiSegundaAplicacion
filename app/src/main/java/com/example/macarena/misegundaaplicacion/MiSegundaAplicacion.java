d974ceb9dc9a6714a382f3da2a6869e415b34122
mcarpin <ellafueunangel@gmail.com>
2018-01-22 13:47:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/macarena/misegundaaplicacion/MiSegundaAplicacion.java
@@ -0,0 +1,13 @@
+package com.example.macarena.misegundaaplicacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MiSegundaAplicacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mi_segunda_aplicacion);
+    }
+}
